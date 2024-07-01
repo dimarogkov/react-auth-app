5ccc66bf70bf6c282209b7061810c60b5e18168a
dima.rogkov <dima.rogkov@cheitgroup.com>
2024-07-01 15:12:01 +0300

    add not found page

--- src/components/pages/NotFoundPage/NotFoundPage.tsx
+++ src/components/pages/NotFoundPage/NotFoundPage.tsx
@@ -4,13 +4,15 @@ import { BtnLink, Text, Title } from '../../ui';
 export const NotFoundPage = () => {
     return (
         <Page>
-            <Title className='mb-2 last:mb-0'>Ooops! Page Not Found</Title>
+            <div className='w-full pt-20 sm:pt-24'>
+                <Title className='mb-4 last:mb-0'>Ooops! Page Not Found</Title>
 
-            <Text className='mb-5 last:mb-0'>
-                This page doesn’t exist or was removed! We suggest you go back to home.
-            </Text>
+                <Text className='mb-6 last:mb-0'>
+                    This page doesn’t exist or was removed! We suggest you go back to home.
+                </Text>
 
-            <BtnLink href='/'>Go Back-Home</BtnLink>
+                <BtnLink href='/'>Go Back-Home</BtnLink>
+            </div>
         </Page>
     );
 };
