799183d49a8914743ce7c239175aaa0604941d4d
dima.rogkov <dimarogkov41475@ukr.net>
2024-07-01 15:12:01 +0300

    add not found page

--- src/components/elements/Header/Header.tsx
+++ src/components/elements/Header/Header.tsx
@@ -1,14 +1,18 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { FaReact } from 'react-icons/fa';
 import cn from 'classnames';
+import { useLocation } from 'react-router-dom';
 
 export const Header = () => {
     const { isAuthenticated } = useAuth0();
+    const { pathname } = useLocation();
+
+    console.log(pathname);
 
     return (
         <header
             className={cn('fixed z-10 top-0 left-0 flex items-center w-full h-16 sm:h-20 shadow-md', {
-                'md:shadow-none': !isAuthenticated,
+                'md:shadow-none': !isAuthenticated && pathname === '/',
                 'bg-white': isAuthenticated,
             })}
         >
