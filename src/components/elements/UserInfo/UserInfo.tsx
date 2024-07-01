cd7a9982c7ae6b2689341485e44bca1b1ac9d4ca
dima.rogkov <dimarogkov41475@ukr.net>
2024-07-01 15:59:07 +0300

    some fix

--- src/components/elements/UserInfo/UserInfo.tsx
+++ src/components/elements/UserInfo/UserInfo.tsx
@@ -1,16 +1,22 @@
+import { useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import { useUserInfo } from '../../../store/user';
 import { REACT_APP_AUTH0_REDIRECT_URI } from '../../../variables';
 import { Btn, Title } from '../../ui';
 
 export const UserInfo = () => {
+    const { userInfo, setUserInfo } = useUserInfo((state) => state);
     const { logout, user } = useAuth0();
 
-    const getUserData = user && Object.keys(user).map((key) => ({ id: crypto.randomUUID(), [key]: user[key] }));
+    useEffect(() => {
+        user && setUserInfo(user);
+    }, [setUserInfo, user]);
+
+    console.log(userInfo);
 
     return (
-        <div className='w-full'>
-            <Title className='mb-2 last:mb-0'>Hi, you login to React Auth App !</Title>
-            <div className='mb-2 last:mb-0'>{JSON.stringify(getUserData)}</div>
+        <div className='w-full pt-20 sm:pt-24'>
+            <Title className='mb-2 last:mb-0'>Hi, {userInfo?.nickname}!</Title>
             <Btn onClick={() => logout({ logoutParams: { returnTo: REACT_APP_AUTH0_REDIRECT_URI } })}>Log Out</Btn>
         </div>
     );
