16e624f7e14a25b949d335db28a72579dd65b3ee
dima.rogkov <dimarogkov41475@ukr.net>
2024-07-02 13:06:45 +0300

    change redirect uri

--- src/variables/index.ts
+++ src/variables/index.ts
@@ -1,3 +1,3 @@
 export const REACT_APP_AUTH0_DOMAIN = 'dev-k86dj7i42jpd2vir.us.auth0.com';
 export const REACT_APP_AUTH0_CLIENT_ID = 'sVSpKxKqsooRb4m9zsshkaqRArBtsJzp';
-export const REACT_APP_AUTH0_REDIRECT_URI = 'http://localhost:3000/react-auth-app';
+export const REACT_APP_AUTH0_REDIRECT_URI = 'https://dimarogkov.github.io/react-auth-app';
